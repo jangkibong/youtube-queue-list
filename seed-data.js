5a37c30c367f03a9bb28a10ac34bfabddb8d70d6
jangkibong <willyka@naver.com>
2026-06-27 23:39:02 +0900

    fix목록 삭제

--- seed-data.js
+++ seed-data.js
@@ -5,24 +5,4 @@ window.HARDCODED_PLAYLIST = [
     //   title: 'Lo-fi 집중 음악',
     //   tags: ['공부', '음악', '고정목록'],
     // },
-    {
-        url: "https://youtu.be/08F4mCbyc2k?si=gGIRda5TInhoehdr",
-        title: "갈 길을 밝히 보이시니 + 내게 강 같은 평화",
-        tags: ["남부경향"],
-    },
-    {
-        url: "https://youtu.be/oOIa3oFqHaY?si=MKfaoHl5tG-9CwNL",
-        title: "보혈 메들리",
-        tags: ["남부경향"],
-    },
-    {
-        url: "https://youtu.be/1TQxzXhW_x0?si=2olc8TNUIUT7sY7k",
-        title: "예수 하나님의 공의 +*기도 : 전능하신 하나님께 나아가는 기도+ 사랑하는 나의 아버지",
-        tags: ["남부경향"],
-    },
-    {
-        url: "https://youtu.be/cXDSEkUswf0?si=B9iflSlTp2uRISoe",
-        title: "이 땅의 황무함을 보소서+*기도 : 나라를 위한 기도 + 충만",
-        tags: ["남부경향"],
-    },
 ];
